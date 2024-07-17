841b2b1dc1626bc9eb0df1849df7d65fac97582f
ocean0s <someone@somewhere.notanemail>
2024-07-17 14:29:07 +0200

    Add Create user component & notification system

--- src/App.js
+++ src/App.js
@@ -1,66 +1,93 @@
 import { Layout, Menu } from "antd";
-import { useState } from "react";
-import { Link, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, Routes, Route, useNavigate } from "react-router-dom";
+import CreateUserComponent from "./Components/CreateUserComponent";
+import { notification as antNotif } from "antd"
+import { backendURL } from "./Globals";
 
 function App() {
 
+  const [api, contextHolder] = antNotif.useNotification()
   let [login, setLogin] = useState(false)
-
+  let navigate = useNavigate()
   let { Header, Content, Footer } = Layout
 
+  let createNotification = (msg, description="", type = "info", placement = "top") => {
+    api[type]({
+      message: msg,
+      description: description,
+      placement
+    })
+  }
+
+  // useEffect( () => {
+  //   setLogin(localStorage.getItem("apiKey") != null) // improve later TODO
+  // }, [])
+
+  let disconnect = async () => {
+    await fetch(backendURL + "/users/disconnect?apiKey=" + localStorage.getItem("apikey"),
+      {method: "POST"})
+    localStorage.removeItem("apiKey")
+    setLogin(false)
+    navigate("/login")
+  }
+
   return (
-    <Layout style={{ minHeight: "100vh"}}>
-      <Header>
-        {!login ? (
-          <Menu theme="dark" mode="horizontal" items={[
-            { key: "menuRegister", label: <Link to="/register">Register</Link>},
-            { key: "menuLogin", label: <Link to="/login">Login</Link>},
-          ]}>
-          </Menu>
-        ) : (
-          <Menu theme="dark" mode="horizontal" items={[
-            { key: "menuIndex", label: <Link to="/">Index</Link>},
-            { key: "menuPresents", label: <Link to="/presents">Presents</Link>},
-            { key: "menuCreatePresent", label: <Link to="/presents/create">Create a present</Link>},
-            { key: "menuFriends", label: <Link to="/friends">Friends</Link>},
-            { key: "menuGift", label: <Link to="/presents/friends">Gift a friend</Link>},
-            { key: "menuDisconnect", label: <Link to="#" /* onClick={disconnect} */>Disconnect</Link>},
-          ]}>
-          </Menu>
-        )}
-      </Header>
-      <Content>
-        <Routes>
-          <Route path="/register" element={
-            <p>Register</p>
-          }></Route>
-          <Route path="/login" element={
-            <p>Login</p>
-          }></Route>
-          <Route path="/" element={
-            <p>Index</p>
-          }></Route>
-          <Route path="/presents" element={
-            <p>Present list</p>
-          }></Route>
-          <Route path="/presents/create" element={
-            <p>Present creation</p>
-          }></Route>
-          <Route path="/present/edit/:id" element={
-            <p>Register</p>
-          }></Route>
-          <Route path="/friends" element={
-            <p>Register</p>
-          }></Route>
-          <Route path="/presents/friends" element={
-            <p>Gift a friend</p>
-          }></Route>
-        </Routes>
-      </Content>
-      <Footer style={{textAlign:"center"}}>
-          Presents App &copy;
-      </Footer>
-    </Layout>
+    <>
+      {contextHolder}
+      <Layout style={{ minHeight: "100vh"}}>
+        <Header>
+          {!login ? (
+            <Menu theme="dark" mode="horizontal" items={[
+              { key: "menuRegister", label: <Link to="/register">Register</Link>},
+              { key: "menuLogin", label: <Link to="/login">Login</Link>},
+            ]}>
+            </Menu>
+          ) : (
+            <Menu theme="dark" mode="horizontal" items={[
+              { key: "menuIndex", label: <Link to="/">Index</Link>},
+              { key: "menuPresents", label: <Link to="/presents">Presents</Link>},
+              { key: "menuCreatePresent", label: <Link to="/presents/create">Create a present</Link>},
+              { key: "menuFriends", label: <Link to="/friends">Friends</Link>},
+              { key: "menuGift", label: <Link to="/presents/friends">Gift a friend</Link>},
+              { key: "menuDisconnect", label: <Link to="#" onClick={disconnect} >Disconnect</Link>},
+            ]}>
+            </Menu>
+          )}
+        </Header>
+        <Content style={{ padding: "20px 50px" }}>
+          <Routes>
+            <Route path="/register" element={
+              <CreateUserComponent createNotification={createNotification}/>
+            }></Route>
+            <Route path="/login" element={
+              <p>Login</p>
+            }></Route>
+            <Route path="/" element={
+              <p>Index</p>
+            }></Route>
+            <Route path="/presents" element={
+              <p>Present list</p>
+            }></Route>
+            <Route path="/presents/create" element={
+              <p>Present creation</p>
+            }></Route>
+            <Route path="/present/edit/:id" element={
+              <p>Register</p>
+            }></Route>
+            <Route path="/friends" element={
+              <p>Register</p>
+            }></Route>
+            <Route path="/presents/friends" element={
+              <p>Gift a friend</p>
+            }></Route>
+          </Routes>
+        </Content>
+        <Footer style={{textAlign:"center"}}>
+            Presents App &copy;
+        </Footer>
+      </Layout>
+    </>
   );
 }
 
---
+++ src/Components/CreateUserComponent.js
@@ -0,0 +1,86 @@
+import { Button, Card, Input, Row, Col, Typography } from "antd"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { backendURL } from "../Globals"
+const {Text} = Typography
+
+let CreateUserComponent = (props) => {
+
+    let { createNotification } = props
+
+    let [email, setEmail] = useState()
+    let [username, setUsername] = useState()
+    let [password, setPassword] = useState()
+
+    let navigate = useNavigate()
+    let [disabled, setDisabled] = useState(false)
+    let [error, setError] = useState({})
+
+    useEffect( () => {
+        checkErrors()
+    }, [username, email, password])
+
+    let checkErrors = () => {
+        let newError = {}
+        if (email != undefined && !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email))
+            newError.email = "Invalid email"
+        if (password != undefined && password.length < 5)
+            newError.password = "Password too short"
+        if (username != undefined && username == "")
+            newError.username = "Username cannot be empty"
+        setError(newError)
+        if (error.email != undefined || error.password != undefined || error.username != undefined)
+            setDisabled(true)
+        else
+            setDisabled(false)
+    }
+
+    let changeEmail = (e) => { setEmail(e.currentTarget.value) }
+    let changeUsername = (e) => { setUsername(e.currentTarget.value) }
+    let changePassword = (e) => { setPassword(e.currentTarget.value) }
+
+    let clickCreate = async () => {
+        let response = await fetch(backendURL+"/users", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" }, 
+            body: JSON.stringify({
+                email: email,
+                username: username,
+                password: password
+            })
+        })
+
+        if (response.ok) {
+            createNotification("User created successfully")
+            navigate("/login")
+        } else {
+            let jsonData = await response.json()
+            if (Array.isArray(jsonData.error)){
+                let error = ""
+                jsonData.error.forEach(element => {
+                    {error += element + "\n"}
+                });
+                createNotification(error)
+            } else 
+                createNotification(jsonData.error)
+        }
+    }
+
+    return (
+        <Row align="middle" justify="center" style={{minHeight: "70vh"}}>
+            <Col>
+                <Card size="default" title="Register" style={{ minWidth: "300px", maxWidth: "500px"}}>
+                    <Input onChange={changeEmail} size="large" type="email" placeholder="Email..." style={{marginBottom: "8px"}}></Input>
+                    {error.email !== undefined && <Text style={{marginBottom: "8px", display:"inline-block"}} type="danger" className="errorForm">{error.email}</Text>}
+                    <Input onChange={changeUsername} size="large" type="text" placeholder="Username..." style={{marginBottom: "8px"}}></Input>
+                    {error.username !== undefined && <Text style={{marginBottom: "8px", display:"inline-block"}} type="danger" className="errorForm">{error.username}</Text>}
+                    <Input onChange={changePassword} size="large" type="text" placeholder="Password..." style={{marginBottom: "8px"}}></Input>
+                    {error.password !== undefined && <Text style={{marginBottom: "8px", display:"inline-block"}} type="danger" className="errorForm">{error.password}</Text>}
+                    <Button disabled={disabled} block type="primary" onClick={clickCreate}>Create account</Button>
+                </Card>
+            </Col>
+        </Row>
+    )
+}
+
+export default CreateUserComponent
