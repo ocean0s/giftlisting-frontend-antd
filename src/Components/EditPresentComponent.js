bbc4529a08abfcbe701bf5ca5074ffde26c6cce0
ocean0s <someone@somewhere.notanemail>
2024-07-17 19:59:20 +0200

    Add edit present component

--- src/Components/EditPresentComponent.js
+++ src/Components/EditPresentComponent.js
@@ -1,64 +1,68 @@
 import { useState, useEffect } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 import { backendURL } from "../Globals"
-import { Descriptions, Row, Col, Button, Card, Input, Typography } from "antd"
+import { Descriptions, Row, Col, Button, Card, Input, Typography, Alert } from "antd"
 const { Text } = Typography
 
 let EditPresentComponent = (props) => {
 
     let {id} = useParams()
     let {createNotification} = props
-
+    let [message, setMessage] = useState("")
     let [item, setItem] = useState({})
-    let [url, setUrl] = useState()
-    let [name, setName] = useState()
-    let [description, setDescription] = useState()
-    let [price, setPrice] = useState()
-
+    let [itemOriginal, setItemOriginal] = useState({})
     let navigate = useNavigate()
     let [disabled, setDisabled] = useState(true)
     let [error, setError] = useState({})
 
     useEffect(() => {
+        getItem()
         setDisabled(true)
     }, [])
 
+    let getItem = async () => {
+        let response = await fetch(backendURL+"/presents/" + id + "?apiKey=" + localStorage.getItem("apiKey"))
+        if (response.ok) {
+            let jsonData = await response.json()
+            setItem(jsonData.present[0])
+            setItemOriginal(jsonData.present[0])
+        } else {
+            setMessage("Error")
+            setItem({})
+        }
+    }
+
     useEffect( () => {
         checkErrors()
-    }, [name, url, description, price])
+    }, [item])
 
     let checkErrors = () => {
         let newError = {}
-        if (url != undefined && url == "")
+        if (item.url != undefined && item.url == "")
             newError.url = "URL cannot be empty"
-        if (description != undefined && description == "")
+        if (item.description != undefined && item.description == "")
             newError.description = "Description cannot be empty"
-        if (name != undefined && name == "")
+        if (item.name != undefined && item.name == "")
             newError.name = "Name cannot be empty"
-        if ((price != undefined && isNaN(price)) || (!isNaN(price) && parseFloat(price) <= 0))
+        if ((item.price != undefined && isNaN(item.price)) || (!isNaN(item.price) && parseFloat(item.price) <= 0))
             newError.price = "Price has to be a positive number"
         setError(newError)
-        if ((error.url != undefined || error.description != undefined || error.name != undefined || error.price != undefined)
-             || url == undefined || description == undefined || name == undefined || price == undefined)
+        if ((newError.url != undefined || newError.description != undefined || newError.name != undefined || newError.price != undefined)
+             || item.url == undefined || item.description == undefined || item.name == undefined || item.price == undefined)
             setDisabled(true)
         else
             setDisabled(false)
     }
 
-    let changeUrl = (e) => { setUrl(e.currentTarget.value) }
-    let changeName = (e) => { setName(e.currentTarget.value) }
-    let changeDescription = (e) => { setDescription(e.currentTarget.value) }
-    let changePrice = (e) => { setPrice(e.currentTarget.value) }
-
-    let clickCreate = async () => {
-        let response = await fetch(backendURL+"/presents?apiKey=" + localStorage.getItem("apiKey"), {
-            method: "POST",
+    let clickEdit = async () => {
+        let response = await fetch(backendURL+"/presents/" + id + "?apiKey=" + localStorage.getItem("apiKey"), {
+            method: "PUT",
             headers: { "Content-Type": "application/json" }, 
             body: JSON.stringify({
-                url: url,
-                name: name,
-                description: description,
-                price: parseFloat(price)
+                url: item.url,
+                name: item.name,
+                description: item.description,
+                price: parseFloat(item.price)
             })
         })
 
@@ -78,22 +82,32 @@ let EditPresentComponent = (props) => {
         }
     }
 
+    let changeProperty = (propertyName, e) => {
+        let itemNew = {...item, [propertyName] : e.currentTarget.value}
+        setItem(itemNew)
+    }
 
     return (
         <>
-            <Descriptions title={item.name} layout="vertical"></Descriptions>
+            {message != "" && <Alert type="error" message={message}/>}
+            <Descriptions title={"Original item description: " + itemOriginal.name} bordered layout="vertical">
+                <Descriptions.Item label="Description" span={3}>{itemOriginal.description}</Descriptions.Item>
+                <Descriptions.Item span={1} label="Price">{itemOriginal.price}</Descriptions.Item>
+                <Descriptions.Item span={2} label="URL">{itemOriginal.url}</Descriptions.Item>
+                <Descriptions.Item label="Chosen by" span={3}>{itemOriginal.chosenBy == null ? "No one yet" : itemOriginal.chosenBy}</Descriptions.Item>
+            </Descriptions>
             <Row align="middle" justify="center" style={{minHeight: "70vh"}}>
                 <Col>
-                    <Card size="default" title="Create a present" style={{ minWidth: "300px", maxWidth: "500px"}}>
-                        <Input onChange={changeName} size="large" type="text" placeholder="Name..." style={{marginBottom: "8px"}}></Input>
+                    <Card size="default" title="Edit present" style={{ minWidth: "300px", maxWidth: "500px"}}>
+                        <Input value={item.name} onChange={(e) => changeProperty("name",e)} size="large" type="text" placeholder="Name..." style={{marginBottom: "8px"}}></Input>
                         {error.name !== undefined && <Text style={{marginBottom: "8px", display:"inline-block"}} type="danger">{error.name}</Text>}
-                        <Input onChange={changeDescription} size="large" type="text" placeholder="Description..." style={{marginBottom: "8px"}}></Input>
+                        <Input value={item.description} onChange={(e) => changeProperty("description",e)} size="large" type="text" placeholder="Description..." style={{marginBottom: "8px"}}></Input>
                         {error.description !== undefined && <Text style={{marginBottom: "8px", display:"inline-block"}} type="danger">{error.description}</Text>}
-                        <Input addonBefore="http://" onChange={changeUrl} size="large" type="text" placeholder="URL..." style={{marginBottom: "8px"}}></Input>
+                        <Input value={item.url} addonBefore="http://" onChange={(e) => changeProperty("url",e)} size="large" type="text" placeholder="URL..." style={{marginBottom: "8px"}}></Input>
                         {error.url !== undefined && <Text style={{marginBottom: "8px", display:"inline-block"}} type="danger">{error.url}</Text>}
-                        <Input onChange={changePrice} size="large" type="number" placeholder="Price..." style={{marginBottom: "8px"}}></Input>
+                        <Input value={item.price} onChange={(e) => changeProperty("price",e)} size="large" type="number" placeholder="Price..." style={{marginBottom: "8px"}}></Input>
                         {error.price !== undefined && <Text style={{marginBottom: "8px", display:"inline-block"}} type="danger">{error.price}</Text>}
-                        <Button disabled={disabled} block type="primary" onClick={clickCreate}>Create present</Button>
+                        <Button disabled={disabled} block type="primary" onClick={clickEdit}>Edit present</Button>
                     </Card>
                 </Col>
             </Row>
